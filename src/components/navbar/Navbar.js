7e6fa9b7883c316bc1e4f0fc004d3f5526d74677
Ishwar2510 <ishwar2510@gmail.com>
2023-01-16 12:07:44 +0530

    first-commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <header>
+        <nav>
+            <div className='left'>
+                <img src="./Amazon_logo.png" alt='logo'/>
+            </div>
+            <div className='right'></div>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
