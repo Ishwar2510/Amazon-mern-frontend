c96199ce4cb19308a7791745091765addcc257db
Ishwar2510 <ishwar2510@gmail.com>
2023-01-17 14:46:38 +0530

    for deployment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
+import Navbaar from './Components/header/Navbaar';
+import Newnav from './Components/newnav/Newnav';
+import Maincomp from './Components/home/Maincomp';
+import Footer from './Components/footer/Footer';
+import Signup from './Components/signup_signin/SignUp';
+import SignIn from './Components/signup_signin/SignIn';
+import Cart from './Components/cart/Cart';
+import Buynow from './Components/buynow/Buynow';
 import './App.css';
-import Navbar from './components/navbar/Navbar';
+import { useEffect, useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
+  const [data, setData] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData(true);
+    }, 2000);
+  }, [])
+
+
   return (
-   <Navbar/>
+    <>
+      {
+        data ? (
+          <>
+            <Navbaar />
+            <Newnav />
+            <Routes>
+              <Route exact path="/" element={<Maincomp/>}/>
+               
+             
+              <Route exact path="/signup" element={<Signup />}/>
+                
+              
+              <Route exact path="/login" element={<SignIn />}/>
+                
+              
+              <Route exact path="/getproductsone/:id" element={<Cart />}/>
+                
+              
+              <Route exact path="/buynow" element={<Buynow />}/>
+                
+              
+            </Routes>
+            <Footer />
+          </>
+        ) : (
+          <div className="circle">
+            <CircularProgress />
+            <h2> Loading....</h2>
+          </div>
+        )
+      }
+
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter } from "react-router-dom";
+import Contextprovider from "./Components/context/Contextprovider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Contextprovider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </Contextprovider>
 );
-
+// ReactDOM.render(
+//   <Contextprovider>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </Contextprovider>
+//   ,
+//   document.getElementById('root')
+// );
