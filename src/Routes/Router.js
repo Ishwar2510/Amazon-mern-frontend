da23b0b47a1fd6c4a32eb79cc356bbcfd8342491
Ishwar2510 <ishwar2510@gmail.com>
2023-01-20 22:28:52 +0530

    code refactored Router and Page Seprated         modified:   src/App.js         new file:   src/Routes/Router.js         new file:   src/pages/BuynowPage.js         new file:   src/pages/HomePage.js         new file:   src/pages/LoginPage.js         new file:   src/pages/ProductPage.js         new file:   src/pages/RegisterPage.js

---
+++ src/Routes/Router.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import BuynowPage from '../pages/BuynowPage'
+
+import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/LoginPage'
+import ProductPage from '../pages/ProductPage'
+import RegisterPage from '../pages/RegisterPage'
+function Router() {
+  return (
+    <>
+        <Routes>
+            <Route path ='/' element = {<HomePage/>}></Route>
+            <Route path = '/signup' element = {<RegisterPage/>}></Route>
+            <Route path = '/login' element = {<LoginPage/>}></Route>
+            <Route path = '/getproductsone/:id' element = {<ProductPage/>}></Route>  
+            <Route path = '/buynow' element = {<BuynowPage/>}></Route>
+        </Routes>
+    </>
+  )
+}
+
+export default Router
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Banner from '../Components/home/Banner'
+import Maincomp from '../Components/home/Maincomp'
+
+function HomePage() {
+  return (
+   <Maincomp/>
+  )
+}
+
+export default HomePage
