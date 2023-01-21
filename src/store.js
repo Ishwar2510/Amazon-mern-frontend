fb5d4159bf3f5f824922356963f33352f97c52c9
Ishwar2510 <ishwar2510@gmail.com>
2023-01-21 16:28:59 +0530

    changed redux store to RTK         modified:   package-lock.json         modified:   package.json         modified:   src/Components/header/Navbaar.js         modified:   src/store.js

--- src/store.js
+++ src/store.js
@@ -1,14 +1,8 @@
-import { createStore,applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools-extension";
-import rootreducers from "./Components/redux/reducers/main";
-
-
-const middleware = [thunk];
-
-const store = createStore(
-    rootreducers,
-    composeWithDevTools(applyMiddleware(...middleware))
-);
-
-export default store;
+import {getProductsReducers} from './Components/redux/reducers/Productreducers'
+import {configureStore} from '@reduxjs/toolkit'
+const store = configureStore({
+    reducer:{
+        getproductsdata : getProductsReducers
+    }
+})
+export default store
